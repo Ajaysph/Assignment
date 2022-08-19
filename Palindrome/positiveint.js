3a1ab7298260459f1aa5dee51081ddd2ee279caa
Ajaysph <ajay601kumar@gmail.com>
2022-08-19 13:09:33 +0530

    Employee Designation

--- Palindrome/positiveint.js
+++ Palindrome/positiveint.js
@@ -1,8 +1,12 @@
 (function(B){
 var a=5;
 var b=6;
+
 var A = a*a + b*b;
+
 A<=B ? console.log("true"): console.log("false");
+
 0<=A<=100000 ? console.log("true") : console.log("false");
+
 
 })(81);
